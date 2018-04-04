49f9288bef8034e494ba13bed28d43fa8cf54b84
Vincent <vtenbrinkhanze@st.hanze.nl>
2018-04-04 11:27:20 +0200

    Added interactive cli play

--- BKE/src/Main.java
+++ BKE/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         TicTacToe TTT = new TicTacToe();
-        TTT.findBestMove(true);
+        Scanner s = new Scanner(System.in);
+
+        while (true) {
+            TTT.findBestMove(true);
+            String move = s.nextLine();
+            String[] split = move.split("");
+            int x = new Integer(split[0]);
+            int y = new Integer(split[1]);
+
+            Move pmove = new Move(x, y, false);
+
+            TTT.playMove(pmove);
+
+        }
+
     }
 }
--- BKE/src/TicTacToe.java
+++ BKE/src/TicTacToe.java
@@ -35,14 +35,27 @@ public class TicTacToe {
     }
 
     /**
+     * Plays a certain move.
+     *
+     * @param Move Move to play
+     */
+    public void playMove(Move Move) {
+        board.playMove(Move);
+        board.print();
+    }
+
+    /**
      * The recursive minimaxing algorithm. DFS throughout the gametree. Determines the worth of a certain move by examining all possible following gamestates.
      * @param currentState Current state of the board.
      * @param Maxer
      * @param count Used to penalize favorable gamestates that are more moves away. Favours a win in a single move over a win in two moves.
      * @return
      */
+
     public int getFinalScore(Board currentState, Boolean Maxer, int count) {
         count++;
+
+        count = 0; //count caused the a.i. to start out with the wrong move (1, 1)
 
         ArrayList<Move> Moves = currentState.getValidMoves(Maxer);
 
