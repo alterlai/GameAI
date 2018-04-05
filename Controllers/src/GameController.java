34d2afffa7dee289f4b5ffc394c11a3ea81934d6
Mitchel <mitchelw62@hotmail.com>
2018-04-05 09:55:47 +0200

    GameController naar huidig UML gemaakt

--- Controllers/src/GameController.java
+++ Controllers/src/GameController.java
@@ -17,20 +17,25 @@ public class GameController implements MainController {
 
     }
 
-    public void BestMove(){
+    public void findMove(){
+        //vraagt het model om een set te doen
         //model.Move();
     }
-    //dan naar server
-    //dan op board
-    //krijg move van enem
-    //set move op board
-    //
 
-    public void Forfeit(){
-        //model.Forfeit();
-        //server.Forfeit();
+    public void moveSucces(){
+        //wanneer de move is goed gekeurd door de server moet dit geupdate worden in de view.
+    }
+    public void setChosenMove(){
+        //welke set heeft te tegenstander gedaan? update het model en view
 
     }
+    public void Forfeit(){
+        //wanneer onze client forfiet moet dit worden doorgestuurd naar de server
+    }
 
+    public void endGame(){
+        //wanneer iemand heeft verloren zal de server deze methode aanroepen wat ervoor zorgt dat de model en view wordt geupdate
+
+    }
 
 }
