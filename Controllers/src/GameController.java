f6139b9881344d8160f62b50c791695a9b493593
Mitchel <mitchelw62@hotmail.com>
2018-04-05 13:47:57 +0200

    Controllers geupdate

--- Controllers/src/GameController.java
+++ Controllers/src/GameController.java
@@ -3,6 +3,9 @@ public class GameController implements MainController {
     //Server server
     //View view
     //Game game
+    //chosenMove
+
+    //Move move;
 
     public GameController(){
 //        this.model = model;
@@ -17,24 +20,38 @@ public class GameController implements MainController {
 
     }
 
-    public void findMove(){
-        //vraagt het model om een set te doen
-        //model.Move();
+    public void findMove() throws InterruptedException {
+//        move=null;
+//        if(playerIsAi){
+//            move = model.getmove();
+//        }
+//        else{
+//            view.yourturn();
+//            wait();
+//
+//        }
+//        return move;
+
     }
 
     public void moveSucces(){
-        //wanneer de move is goed gekeurd door de server moet dit geupdate worden in de view.
-    }
-    public void setChosenMove(){
-        //welke set heeft te tegenstander gedaan? update het model en view
+        //wanneer de move is goed gekeurd door de server moet dit geupdate worden in het gamemodel.
+        //game.player(move);
 
     }
+
     public void Forfeit(){
         //wanneer onze client forfiet moet dit worden doorgestuurd naar de server
     }
 
     public void endGame(){
         //wanneer iemand heeft verloren zal de server deze methode aanroepen wat ervoor zorgt dat de model en view wordt geupdate
+
+    }
+
+    public void  registermove(){
+        //update de move variable in de controller;
+        //this.move = move;
 
     }
 
--- Controllers/src/LobbyController.java
+++ Controllers/src/LobbyController.java
@@ -1,13 +1,21 @@
+import Server.Server;
+
+import java.io.IOException;
+
 public class LobbyController implements MainController {
     //Model model
-    //Server server
+    Server server;
     //View view
 
     public LobbyController(){
+
 //        this.model = model;
 //        view =;
 //        view.createview();
 //        model.init();
+        this.server = new Server();
+        new Thread(server).start();
+        server.run();
     }
 
     @Override
@@ -15,12 +23,24 @@ public class LobbyController implements MainController {
 
     }
 
-    public void ChallangePlayer(String Game, String PlayerName){
-        //server.ChallangePlayer(Game, PlayerName);
+    public void Challenge(String Tegenstander, String Game){
+        //view.challenge(Tegenstander, Game);
     }
 
-    public void SubscribeGame(String Game){
-        //server.SubscribeGame(Game);
+    public void ChallengePlayer(String Game, String PlayerName) throws IOException {
+        server.challenge(PlayerName, Game);
+        //update view dat we een speller hebben uitgedaagt
+    }
+
+    public void acceptChallenge(){
+        //server.acceptchallenge();
+    }
+
+    public void SubscribeGame(String Game) throws IOException {
+        if(server.subscribe(Game)){
+            //update view dat we in queue zitten
+        }
+
     }
 
 }
