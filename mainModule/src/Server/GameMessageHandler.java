e88e868e514c64fe28290a5248235831ca3f3f9b
woesterudolf <rudolfklijnhout@hotmail.com>
2018-04-11 16:11:13 +0200

    isAi boolean

--- mainModule/src/Server/GameMessageHandler.java
+++ mainModule/src/Server/GameMessageHandler.java
@@ -85,10 +85,12 @@ public class GameMessageHandler implements MessageHandlerInterface {
     private static void setupMatch(String message){
         LobbyObservable.getInstance().stop();
         ArrayList<String> list = new ArrayList<String>(Arrays.asList(message.substring(16, message.length() - 1).split(",")));
-        Player player1 = new Player(Server.getInstance().getPlayerName(), false);
+        Player player1 = new Player(Server.getInstance().getPlayerName(), isAI);
         Player player2 = new Player(list.get(2).substring(12, list.get(2).length()-1));
         String nameGame = list.get(1).substring(12, list.get(1).length()-1);
         GameHandler handler = GameHandler.getInstance();
         handler.initGameController(player1, player2, nameGame);
     }
+
+    public static void setisAI(Boolean isAI){GameMessageHandler.isAI = isAI;}
 }
