d656dd023828718c719d2e264c978a461f0c83ec
woesterudolf <rudolfklijnhout@hotmail.com>
2018-04-17 09:50:45 +0200

    Revert "Revert "more docs""
    This reverts commit 5ad6e19d745a1d92f32783d5d0a2460c2ef807ba.

--- mainModule/src/Server/MessageHandler.java
+++ mainModule/src/Server/MessageHandler.java
@@ -2,10 +2,23 @@ package Server;
 
 import java.io.BufferedReader;
 
+/**
+ * <H1> MessageHandler</H1>
+ * This class handels messages and passes them to the appropiate handler
+ * @author Rudolf Klijnhout
+ * @version 1.0
+ * @since 16-04-2018
+ **/
+
 public class MessageHandler implements MessageHandlerInterface {
 
     private static boolean lastCommandAnswer = false;
 
+    /**
+     * This method is used to handle the messages
+     * @param message String server message
+     * @throws Exception if the is a unkown message
+     */
     public static void handleMessage(String message) throws Exception {
         if(message.startsWith("SVR GAME")) {
             GameMessageHandler.handleMessage(message);
@@ -22,6 +35,11 @@ public class MessageHandler implements MessageHandlerInterface {
         }
     }
 
+    /**
+     * This method is used to wait for an ok after a command and handels in between messages
+     * @param dataIn BufferedReader that is the dataIn for the connection
+     * @throws Exception if it is not able to read a new line
+     */
     public static void waitForOk(BufferedReader dataIn) throws Exception {
         String data = dataIn.readLine();
         while (!data.equals("OK")) {
